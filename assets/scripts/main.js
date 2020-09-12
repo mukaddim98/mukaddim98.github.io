27990e2c0140dcc6ae2fb6b9ebdc7af817535019
mukaddim98 <mukaddim1998@gmail.com>
2020-09-12 17:43:43 -0300

    Fixed media width issue

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,9 @@
-function myFunction(x) {
-  if (x.matches) {
-    document.getElementById("hello2").innerHTML = "Hello";
-    console.log(x);
+function displayChange() {
+  if (document.body.clientWidth < "530") {
+    document.getElementById("hel1").classList.remove("display-1");
+    document.getElementById("hel2").classList.remove("display-3");
+  } else {
+    document.getElementById("hel1").classList.add("display-1");
+    document.getElementById("hel2").classList.add("display-3");
   }
 }
-
-var x = window.matchMedia("(max-width: 530px)");
-myFunction(x); // Call listener function at run time
-x.addListener(myFunction); // Attach listener function on state changes
