acbf1c3c9429270617470607623b7e114bb6abb6
mukaddim98 <mukaddim1998@gmail.com>
2020-09-12 18:19:22 -0300

    Reverted back

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,13 @@
 function displayChange() {
   if (document.body.clientWidth < "530") {
     document.getElementById("hel1").classList.remove("display-1");
+    document.getElementById("hel1").classList.add("display-5");
     document.getElementById("hel2").classList.remove("display-3");
+    document.getElementById("hel2").classList.add("display-6");
   } else {
+    document.getElementById("hel1").classList.remove("display-5");
     document.getElementById("hel1").classList.add("display-1");
+    document.getElementById("hel2").classList.remove("display-6");
     document.getElementById("hel2").classList.add("display-3");
   }
 }
